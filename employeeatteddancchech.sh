fb5864c43420ddb726a8df3f7f00e61a8e5df1bc
arpantubai <arpankng93@gmail.com>
2022-05-02 18:44:26 +0530

    [Arpan] : Addded implemented EmployeeAttendanc in Employeewages

--- employeeatteddancchech.sh
+++ employeeatteddancchech.sh
@@ -77,3 +77,20 @@ totalsalary=$(( $totalsalary + $salary ))
 done
 #totalsalary=$(( $totalsalary + $salary ))
 echo "Total salary of month :" $totalsalary
+
+#UC6
+Total_Working_Hr=100
+Total_Hr=0
+Total_Day=0
+Total_Salary=0
+while [ $Total_Hr -lt $Total_Working_Hr -a $Total_Day -lt $numofWorkingDays ]
+do
+        (( Total_Day++ ))
+        Total_Hr=$(( $Total_Hr + $Emp_Hr ))
+        echo "Total hour : " $Total_Hr
+        salary=$(( $Total_Hr * $Wage_Per_Hr ))
+        Total_Salary=$(( $Total_Salary + $salary ))
+
+done
+echo "Total hour : " $Total_Hr
+echo "Total employee wage per hour and month :" $Total_Salary
