2efdd9ab3d991afcb10836a5dbc5ded3d26988a8
arpantubai <arpankng93@gmail.com>
2022-05-02 18:24:04 +0530

    [Arpan] : Addded implemented EmployeeAttendanc in Employeewages

--- employeeatteddancchech.sh
+++ employeeatteddancchech.sh
@@ -19,3 +19,10 @@ Full_Day_Hr=8
 
 dailyWage=$(( $Wage_Per_Hr * $Full_Day_Hr ))
 echo "Daily Employee Wage :" $dailyWage
+
+#UC3
+
+Part_Time_Hr=4
+partTimeWage=$(( $Part_Time_Hr * $Wage_Per_Hr ))
+echo "Part time wage of an employee :" $partTimeWage
+
